082f157c25512d9d89f1a0966e2dbf42322378c6
millertim83 <80060826+millertim83@users.noreply.github.com>
2021-08-25 11:43:18 -0400

    added functions to draw apple in random location and move snake with arrow keys.

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
 let canvas;
 let canvasContext;
 
-let snakePositionX = 50
-
+let snakeDirection = ''
+let snakeBody = [
+    {x: 50, y: 50},
+    {x: 70, y: 50}
+]
+let randomX = getRandomX();
+let randomY = getRandomY();
 
 window.onload = function() {
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
     
-    setInterval(drawEverything, 50);
+    setInterval(drawEverything, 250);
+    setInterval(moveSnake, 250);
+    getRandomX();
+    getRandomY();
     
     
 }
-
-
-
 function drawEverything() {
-    snakePositionX = snakePositionX + 7;
-    
     //color canvas
     colorRect(0, 0, canvas.width, canvas.height, 'gray');
 
     //draw snake
-    colorRect(snakePositionX, 200, 40, 15, 'green');
-
+    snakeBody.forEach(snakeBodyLink => {
+        colorRect(snakeBodyLink.x, snakeBodyLink.y, 20, 20, 'lime');
+    });
+    
     //draw apple
-    colorCircle(200, 400, 5, 'yellow');
+    colorCircle(randomX, randomY, 5, 'yellow');
 }
 
 function colorCircle(centerX, centerY, radius, drawColor) {
@@ -40,6 +45,70 @@ function colorRect(leftX, topY, width, height, drawColor) {
     canvasContext.fillRect(leftX, topY, width, height);
 }
 
-function moveSnake() {
-    snakePositionX = snakePositionX + 10;
+function getRandomX() {
+    return Math.floor((Math.random() * 800));
 }
+function getRandomY() {
+    return Math.floor((Math.random() * 600));
+}
+
+document.addEventListener('keydown', determineSnakeDirection);
+
+function determineSnakeDirection(e) {
+    if (e.code == 'ArrowLeft') {
+        if (snakeDirection ==='right') {
+            return;
+        }
+        snakeDirection = 'left';
+        console.log('moving left');
+    }
+    
+    if (e.code == 'ArrowUp') {
+        if (snakeDirection === 'down') {
+            return;
+        }
+        snakeDirection = 'up';
+        console.log('moving up');
+    }
+    
+    if (e.code == 'ArrowRight') {
+        if (snakeDirection === 'left') {
+            return;
+        }
+        snakeDirection = 'right';
+        console.log('moving right');
+    }
+    
+    if (e.code == 'ArrowDown') {
+        if (snakeDirection === 'up') {
+            return;
+        }
+        snakeDirection = 'down';
+        console.log('moving down');
+    }
+}
+
+function moveSnake() {
+    for (let i = snakeBody.length-1; i > 0; i--) {
+        snakeBody[i] = Object.assign(snakeBody[i], snakeBody[i-1]);
+    }
+    switch(snakeDirection) {
+        case 'left': snakeBody[0].x = snakeBody[0].x - 20;
+                     snakeBody[0].y = snakeBody[0].y;
+                break;
+
+        case 'up': snakeBody[0].y = snakeBody[0].y - 20;
+                   snakeBody[0].x = snakeBody[0].x;
+                break;
+
+        case 'right': snakeBody[0].x = snakeBody[0].x + 20;
+                      snakeBody[0].y = snakeBody[0].y;
+                break;
+
+        case 'down': snakeBody[0].y = snakeBody[0].y + 20;
+                     snakeBody[0].x = snakeBody[0].x;
+    }
+};
+
+
+
